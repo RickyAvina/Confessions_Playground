f5d14271f64ab2539eec87950a6e992a510dbfad
Enrique Avina <rickyavina@icloud.com>
2022-08-21 10:36:23 -0400

    Initial Commit

---
+++ Confessions/ConfessionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfessionsApp.swift
+//  Confessions
+//
+//  Created by Enrique Avina on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ConfessionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Confessions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Confessions
+//
+//  Created by Enrique Avina on 8/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
